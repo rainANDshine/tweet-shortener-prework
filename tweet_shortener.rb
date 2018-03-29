ec683396c9577ef296fcab4b7c376bac36efe706
Shun Yao <ysmiracle@hotmail.com>
2018-03-29 07:15:22 +0000

    automatically backed up by learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -29,3 +29,17 @@ def bulk_tweet_shortener(tweets)
   end
 end
 
+def selective_tweet_shortener(tweet)
+  if tweet.length > 140
+    word_substituter(tweet)
+  else
+    tweet
+  end
+end
+
+
+
+
+
+
+
